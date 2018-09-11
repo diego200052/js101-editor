b5242e6a75fe6ef29256089bb50c618bce48e615
shadowlap <david@ipsilondev.com>
2018-09-11 03:02:09 -0300

    fixing local storage listing bug

---
+++ .quasar/client-entry.js
@@ -0,0 +1,64 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding initialization code.
+ * Use "quasar new plugin <name>" and add it there.
+ * One plugin per concern. Then reference the file(s) in quasar.conf.js > plugins:
+ * plugins: ['file', ...] // do not add ".js" extension to it.
+ **/
+
+
+
+import 'quasar-extras/roboto-font/roboto-font.css'
+
+import 'quasar-extras/material-icons/material-icons.css'
+
+
+
+
+import 'quasar-app-styl'
+
+
+import 'src/css/app.styl'
+
+
+import Vue from 'vue'
+import createApp from './app.js'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const { app, router } = createApp()
+
+
+
+
+
+
+
+
+
+
+
+
+new Vue(app)
+
+
+
+
+
+

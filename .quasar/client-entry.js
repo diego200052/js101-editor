f6a30b8744253323664906007df2b9a6f791e9e0
shadowlap <david@ipsilondev.com>
2018-09-11 15:19:54 -0300

    Commiting production build

--- .quasar/client-entry.js
+++ .quasar/client-entry.js
@@ -39,13 +39,6 @@ import createApp from './app.js'
 
 
 
-Vue.config.devtools = true
-Vue.config.performance = true
-Vue.config.productionTip = false
-
-
-
-console.info('[Quasar] Running SPA with MAT theme.')
 
 
 
